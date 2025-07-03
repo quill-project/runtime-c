738dec46137349ebe25a77a9b59ea07685eed359
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2025-07-03 14:46:15 +0300

    Flush standard output on 'print', 'eprint' and 'panic'

--- src-c/io.c
+++ src-c/io.c
@@ -4,13 +4,16 @@
 
 void quill_print(quill_string_t text) {
     fwrite(text.data, sizeof(uint8_t), text.length_bytes, stdout);
+    fflush(stdout);
 }
 
 void quill_eprint(quill_string_t text) {
     fwrite(text.data, sizeof(uint8_t), text.length_bytes, stderr);
+    fflush(stdout);
 }
 
 void quill_panic(quill_string_t reason) {
     fwrite(reason.data, sizeof(uint8_t), reason.length_bytes, stderr);
+    fflush(stdout);
     exit(1);
 }
